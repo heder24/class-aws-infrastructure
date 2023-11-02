68068bb8d6c1140cc854f0d3b0477f8b8a3c9a63
heder redon <hederredon@gmail.com>
2023-11-01 21:53:36 -0400

    Updated modules in main.tf  file

--- test/locals.tf
+++ test/locals.tf
@@ -6,21 +6,21 @@ locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
-    Name    = local.name
+    Name       = local.name
     GithubRepo = "heder-class-devops"
     GithubOrg  = "hederdevops"
   }
-# domain_name = "hederdevops.com"
-  user_data = <<-EOT
+  # domain_name = "hederdevops.com"
+  user_data                 = <<-EOT
     #!/bin/bash
     sudo apt update -y 
     sudo apt install nginx -y 
     echo "Welcome to My Default Page. Please Deploy Your App" >> /var/www/html/index.html
     echo "My Health Page" >> /var/www/html/health.html
   EOT
- domain = var.domain_name
-  domain_name = local.domain
-  zone_id= data.aws_route53_zone.heder_lab_zone.id
+  domain                    = var.domain_name
+  domain_name               = local.domain
+  zone_id                   = data.aws_route53_zone.heder_lab_zone.id
   use_existing_route53_zone = true
 }
 
