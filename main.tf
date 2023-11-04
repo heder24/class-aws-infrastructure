7c7bea36a2602c7ee7bbd9ee19c6a0a7db9f87bc
heder redon <hederredon@gmail.com>
2023-11-03 22:31:23 -0400

    updated main .tf

--- main.tf
+++ main.tf
@@ -43,23 +43,3 @@ output "web-address" {
   value = aws_instance.web.public_dns
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-output "ami_id" {
-
-  value = data.aws_ami.ubuntu.id
-}
